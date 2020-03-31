9917f204c425a27c2257e62ae82a920e95443178
Sandy <tottati@hotmail.com>
2020-03-31 03:00:20 +0200

    store authenticated user in firestore db, sign in, sign up

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInSignUp from './pages/signin-and-signup/signin-and-signup.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
     state = { currentUser: null };
@@ -16,8 +16,22 @@ class App extends React.Component {
     componentDidMount() {
         // firebase provides persistance user session
         // open subscribtion between firebase and the app
-        this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-            this.setState({ currentUser: user });
+        this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+            if (userAuth) {
+                const userRef = await createUserProfileDocument(userAuth);
+                // documentSnapshot allows to check if a document exists and get data from it. Listening snapshot change.
+                userRef.onSnapshot(snapShot => {
+                    this.setState({
+                        currentUser: {
+                            id: snapShot.id,
+                            ...snapShot.data(),
+                        },
+                    });
+                });
+            } else {
+                // set user to null
+                this.setState({ currentUser: userAuth });
+            }
         });
     }
 
---
+++ src/components/sign-up/sign-up.component.jsx
@@ -0,0 +1,92 @@
+import React from 'react';
+import FormInput from '../form-input/form-input.component';
+import CustomButtom from '../custom-button/custom-button.component';
+import { auth, createUserProfileDocument } from '../../firebase/firebase.utils';
+
+import './sign-up.styles.scss';
+
+class SignUp extends React.Component {
+    state = {
+        displayName: '',
+        email: '',
+        password: '',
+        confirmPassword: '',
+    };
+
+    handleSubmit = async event => {
+        event.preventDefault();
+        const { displayName, email, password, confirmPassword } = this.state;
+        if (password !== confirmPassword) {
+            alert("Passwords don't match");
+            return;
+        }
+        try {
+            const { user } = await auth.createUserWithEmailAndPassword(
+                email,
+                password
+            );
+            createUserProfileDocument(user, { displayName });
+            // clear form
+            this.setState({
+                displayName: '',
+                email: '',
+                password: '',
+                confirmPassword: '',
+            });
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    handleChange = event => {
+        const { value, name } = event.target;
+        this.setState({ [name]: value });
+    };
+
+    render() {
+        const { displayName, email, password, confirmPassword } = this.state;
+        return (
+            <div className='sign-up'>
+                <h2>I do not have an account </h2>
+                <span>Sign up with your email and password</span>
+                <form className='sign-up-form' onSubmit={this.handleSubmit}>
+                    <FormInput
+                        name='displayName'
+                        type='text'
+                        value={displayName}
+                        label='Name'
+                        onChange={this.handleChange}
+                        required
+                    />
+                    <FormInput
+                        name='email'
+                        type='email'
+                        value={email}
+                        label='Email'
+                        onChange={this.handleChange}
+                        required
+                    />
+                    <FormInput
+                        name='password'
+                        type='password'
+                        value={password}
+                        label='Password'
+                        onChange={this.handleChange}
+                        required
+                    />
+                    <FormInput
+                        name='confirmPassword'
+                        type='password'
+                        value={confirmPassword}
+                        label='Confirm password'
+                        onChange={this.handleChange}
+                        required
+                    />
+                    <CustomButtom type='submit'>Sign Up</CustomButtom>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default SignUp;
